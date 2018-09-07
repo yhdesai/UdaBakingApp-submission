67104d6fc10f3c7054ed2780b70cf848dc25df8c
yash <yhdesai99@ymail.com>
2018-09-08 05:20:50 +0530

    submission

--- app/src/main/java/io/github/yhdesai/udabakingapp/RecipeIngredientProvider.java
+++ app/src/main/java/io/github/yhdesai/udabakingapp/RecipeIngredientProvider.java
@@ -28,6 +28,7 @@ public class RecipeIngredientProvider extends AppWidgetProvider {
          ingreTitle = Stash.getString("INGRE_TITLE");
          ingreString = Stash.getString("INGRE_CONTENT");
          views.setTextViewText(R.id.appwidget_title, ingreTitle);
+         //TODO fix this
          views.setTextViewText(R.id.appwidget_ingredients, ingreString);
        //  Log.d("updateAppWidgetIngre", ingre);
 
